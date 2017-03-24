032db70e657d69725866f2489b47dc947dc40277
FedeRob <federob7@gmail.com>
2017-03-24 15:56:47 -0300

    Fix reject promises function

--- services/authService.js
+++ services/authService.js
@@ -5,63 +5,64 @@ angular.module('auth.services', [])
     .factory('AuthenticationService',
         ['$http', '$q',
             function ($http, $q) {
+                var path ='http://localhost:3000/api';
                 var service = {};
 
                 service.Login = function (email, password) {
                     var deferred = $q.defer();
-                    $http.post('http://localhost:3000/api/auth', {email: email, password: password})
+                    $http.post(path + '/auth', {email: email, password: password})
                         .then(function (response) {
                             deferred.resolve(response);
                             console.log('Response from server: ' + response);
                         })
-                        ,function (error) {
+                        .catch(function (error) {
                             deferred.reject(error);
                             console.log('Some error as occurred!: ' + error);
-                        };
+                        });
                     return deferred.promise;
                 };
 
                 service.CreateUser = function (user) {
                     var deferred = $q.defer();
-                    $http.post('http://localhost:3000/api/users', user)
+                    $http.post(path +'/users', user)
                         .then(function (response) {
                             deferred.resolve(response);
                             console.log('User saved:' + response);
                         })
-                        ,function (error) {
+                        .catch(function (error) {
                             deferred.reject(error);
                             console.log('Some error as occurred: ' + error)
-                        };
+                        });
                     return deferred.promise;
 
                 };
 
                 service.GetAll = function (token) {
                     var deferred = $q.defer();
-                    $http.get('http://localhost:3000/api/users',{ headers: {'x-access-token': token} })
+                    $http.get(path + '/users',{ headers: {'x-access-token': token} })
                         .then(function (response) {
                             deferred.resolve(response.data);
                             console.log(response);
                         })
-                        ,function (error) {
+                        .catch(function (error) {
                             deferred.resolve(error);
                             console.log('Some error as occurred: ' + error)
-                        };
+                        });
                     return deferred.promise;
 
                 };
 
                 service.GetOne = function (id) {
                     var deferred = $q.defer();
-                    $http.get('http://localhost:3000/api/users', id)
+                    $http.get(path + '/users', id)
                         .then(function (response) {
                             deferred.resolve(response);
                             console.log(response);
                         })
-                        ,function (error) {
+                        .catch(function (error) {
                             deferred.reject(error);
                             console.log('Some error as occurred: ' + error)
-                        }
+                        });
                     return deferred.promise;
                 };
                 
